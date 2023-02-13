00cbe39e5269e0d74b594deb924183623b826721
andyjh86 <34996730+andyjh86@users.noreply.github.com>
2023-02-13 22:04:25 +1000

    Update and rename Main python code to Main python code.py
    Updated the code so that it filters out any responses that have previously been downvoted
    The bot will make 5 attempts to respond in a way which has not been downvoted before by increasing the temperature of it's attempted response each time.

--- Main python code.py
+++ Main python code.py
@@ -20,34 +20,59 @@ def get_response(event=None):
     input_ids = tokenizer.encode(message + tokenizer.eos_token, return_tensors="pt")
     # concatenate new user input with chat history (if there is)
     bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if len(chat_history_ids) > 0 else input_ids
-    # generate a bot response
-    while True:
+
+    # generate a bot response with filter
+    attempts = 5
+    response_scores = []
+    for attempt in range(attempts):
+        temperature = 0.75 + attempt * 0.1
         chat_history_ids = model.generate(
             bot_input_ids,
             max_length=1000,
             do_sample=True,
             top_k=100,
-            temperature=0.75,
+            temperature=temperature,
             pad_token_id=tokenizer.eos_token_id
         )
         # print the output
         response_text = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        # Check if the response has been previously downvoted
         if response_text not in downvoted_responses:
+            # Insert the response in the text widget
+            conversation.insert(tk.END, f"Bot: {response_text}\n")
+            response_scores.append([response_text, attempt, 0])
             break
-    # Insert the response in the text widget
-    conversation.insert(tk.END, f"Bot: {response_text}\n")
+    else:
+        # No acceptable response, select the one with lowest downvotes
+        response_scores = []
+        for response in downvoted_responses:
+            score = [response, attempts, 0]
+            with open("C:/Zoo/Voting/votes.csv", 'r', newline='') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row[:2] == [message, response]:
+                        upvotes = int(row[2])
+                        downvotes = int(row[3])
+                        score[2] = downvotes
+                        break
+            response_scores.append(score)
+        response_scores.sort(key=lambda s: (s[1], s[2]))
+        response_text = response_scores[0][0]
+        # Insert the response in the text widget
+        conversation.insert(tk.END, f"Bot: {response_text} (using fallback)\n")
+
     # Clear the entry box
     entry.delete(0, tk.END)
     # Move the view to the end of the text widget
     conversation.see(tk.END)
+
     # Save and load upvotes for the bot's responses
     voting_file = "C:/Zoo/Voting/votes.csv"
     response = [message, response_text]
     if os.path.isfile(voting_file):
         with open(voting_file, 'r', newline='') as f:
             reader = csv.reader(f)
-            for row in reader:
+            rows = list(reader)
+            for row in rows:
                 if row[:2] == response:
                     upvotes = int(row[2])
                     downvotes = int(row[3])
@@ -61,42 +86,47 @@ def get_response(event=None):
         with open(voting_file, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(['Input', 'Response', 'Upvotes', 'Downvotes'])
-    # enable upvote and downvote buttons
-    upvote_button.config(state='normal')
-    downvote_button.config(state='normal')
 
-    # wait for upvote or downvote button to be clicked
-    button_clicked = None
-    def on_click(button):
-        nonlocal button_clicked
-        button_clicked = button
+    def write_votes(rows):
+        with open(voting_file, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Input', 'Response', 'Upvotes', 'Downvotes'])
+            writer.writerows(rows)
 
-    upvote_button.config(command=lambda: on_click(upvote_button))
-    downvote_button.config(command=lambda: on_click(downvote_button))
+    def on_upvote():
+        with open(voting_file, 'r', newline='') as f:
+            reader = csv.reader(f)
+            rows = list(reader)
+            for i, row in enumerate(rows):
+                if row[:2] == response:
+                    rows[i][2] = str(int(rows[i][2]) + 1)
+                    write_votes(rows)
+                    break
+            else:
+                rows.append([message, response_text, '1', '0'])
+                write_votes(rows)
 
-    while button_clicked is None:
-        root.update()
+    def on_downvote():
+        downvoted_responses.add(response_text)
+        with open(voting_file, 'r', newline='') as f:
+            reader = csv.reader(f)
+            rows = list(reader)
+            for i, row in enumerate(rows):
+                if row[:2] == response:
+                    rows[i][3] = str(int(rows[i][3]) + 1)
+                    write_votes(rows)
+                    break
+            else:
+                rows.append([message, response_text, '0', '1'])
+                write_votes(rows)
 
-    upvote_button.config(command=lambda: None)
-    downvote_button.config(command=lambda: None)
-    if button_clicked == upvote_button:
-         if upvotes >= downvotes:
-             upvotes += 1
-         else:
-            upvotes += 1
-            downvotes -= 1
-    elif button_clicked == downvote_button:
-         if downvotes >= upvotes:
-             downvotes += 1
-         else:
-           downvotes += 1
-           upvotes -= 1
-    with open(voting_file, 'a', newline='') as f:
-         writer = csv.writer(f)
-         writer.writerow(response + [upvotes, downvotes])
-     # Disable upvote and downvote buttons
-    upvote_button.config(state='disabled')
-    downvote_button.config(state='disabled')
+    # Update the upvote and downvote button commands
+    upvote_button.config(command=on_upvote)
+    downvote_button.config(command=on_downvote)
+
+    # Move the view to the end of the text widget
+    conversation.see(tk.END)
+
 
  # Create the main window
 root = tk.Tk()
@@ -116,9 +146,9 @@ button = tk.Button(root, text="Get Response", command=get_response)
 button.pack()
 
 # Create upvote and downvote buttons
-upvote_button = tk.Button(root, text='Upvote', state='disabled')
+upvote_button = tk.Button(root, text='Upvote')
 upvote_button.pack(side='left')
-downvote_button = tk.Button(root, text='Downvote', state='disabled')
+downvote_button = tk.Button(root, text='Downvote')
 downvote_button.pack(side='right')
 
  # bind the enter key to the entry box
